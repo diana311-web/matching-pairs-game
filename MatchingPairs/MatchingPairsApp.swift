421bde003c2031fefb31204251abcae94a78a6c5
Elena Diana Morosanu <diana.morosanu@endava.com>
2024-06-27 10:07:20 +0300

    Initial Commit

---
+++ MatchingPairs/MatchingPairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingPairsApp.swift
+//  MatchingPairs
+//
+//  Created by Elena Diana Morosanu on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchingPairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
